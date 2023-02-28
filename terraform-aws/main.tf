73b1d6dbe1c922f4c0673a65b95d485cb1cad748
Vitaly Lobachevsky <vlobachevsky@gmail.com>
2023-02-28 20:54:24 +0300

    Fix vpc id

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -25,9 +25,10 @@ resource "aws_vpc" "my_vpc" {
 
 # Create subnets
 resource "aws_subnet" "public_1a" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "Public-1A"
@@ -35,9 +36,10 @@ resource "aws_subnet" "public_1a" {
 }
 
 resource "aws_subnet" "public_1b" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "Public-1B"
