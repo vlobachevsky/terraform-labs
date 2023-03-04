fdb89735132829d825f50f70995389b4b8935110
Vitaly Lobachevsky <vlobachevsky@gmail.com>
2023-03-04 19:32:04 +0300

    Add EC2 instance to public network

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -169,9 +169,6 @@ resource "aws_instance" "host_1a" {
   systemctl start httpd
   systemctl enable httpd
   INTERFACE=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/)
-  SUBNETID=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/${INTERFACE}/subnet-id)
-  echo '<center><h1>This instance is in the subnet wih ID: SUBNETID </h1></center>' > /var/www/html/index.txt
-  sed "s/SUBNETID/$SUBNETID/" /var/www/html/index.txt > /var/www/html/index.html
   EOF
 
   tags = {
