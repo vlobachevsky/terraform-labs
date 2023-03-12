a316c8e103aca8f291c347c28d25a4912eb1fbc3
Vitaly Lobachevsky <vlobachevsky@gmail.com>
2023-03-12 16:51:29 +0300

    Debug

--- terraform-aws/vpc-peering/main.tf
+++ terraform-aws/vpc-peering/main.tf
@@ -180,20 +180,6 @@ resource "aws_security_group" "vpcpeer_prod" {
   }
 }
 
-# Add route to the peering connection to default route table
-resource "aws_default_route_table" "my_vpc_mgmt_default" {
-  default_route_table_id = aws_vpc.my_vpc_mgmt.default_route_table_id
-
-  route {
-    cidr_block                = "10.1.0.0/16"
-    vpc_peering_connection_id = aws_vpc_peering_connection.owner.id
-  }
-
-  tags = {
-    Name = "MAIN"
-  }
-}
-
 # Create internet gateway
 resource "aws_internet_gateway" "my_igw_mgmt" {
   vpc_id = aws_vpc.my_vpc_mgmt.id
